05c91b499418b874aa23c0c5e8526f13fc3249ef
nassreddineEsprit <135167103+nassreddineEsprit@users.noreply.github.com>
2024-07-18 12:23:31 +0100

    Ms-SharedLibrary

---
+++ Ms-SharedLibrary/src/main/java/tn/esprit/mssharedlibrary/dto/JustificationDTO.java
@@ -0,0 +1,19 @@
+package tn.esprit.mssharedlibrary.dto;
+
+import lombok.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class JustificationDTO {
+    private String idJustification;
+    private String justificationName;
+    private String justificationReason;
+    private int justificationStatus;
+    private Date justificationSubmissionDate;
+    private Date justificationValidationDate;
+    private String justificationFilePath;
+}
